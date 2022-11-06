16b820f77b4445b988482f15389b4c0390876a01
Francis Pelletier <francispelletier2@hotmail.com>
2022-11-06 16:29:03 -0500

    Add all functionality (all test pass)

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,130 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.scss";
 
 function App() {
   const [breakLength, setBreakLength] = useState<number>(5);
   const [sessionLength, setSessionLength] = useState<number>(25);
-  const [timer, setTimer] = useState<string>("25:00");
-  const [intervalId, setIntervalId] = useState<number>(-1);
+  const [sessionLengthInSec, setSessionLengthInSec] = useState<number>(
+    sessionLength * 60
+  );
+  const [breakLengthInSec, setBreakLengthInSec] = useState<number>(
+    breakLength * 60
+  );
+  const [timerDisplay, setTimerDisplay] = useState<string>("25:00");
+  const [isBreak, setIsBreak] = useState<boolean>(false);
+  const [start, setStart] = useState<boolean>(false);
+  const [timeoutId, setTimeoutId] = useState<number>(-1);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
-    if (timer === "00:00") {
-      clearInterval(intervalId);
-      updateTimer(sessionLength);
-      setIntervalId(-1);
+    if (start && !isBreak && sessionLengthInSec === 0) {
+      setIsBreak(true);
+      setSessionLengthInSec(() => sessionLength * 60);
+      updateTimerDisplay(breakLengthInSec);
+      audioRef.current && audioRef.current.play();
     }
-    if (intervalId === -1) {
-      updateTimer(sessionLength);
-    }
-  });
 
-  function updateTimer(timeInMin: number): void {
-    const newTime: string = new Date(timeInMin * 60 * 1000)
+    if (start && !isBreak && sessionLengthInSec > 0) {
+      const timeout: number = setTimeout(
+        () =>
+          setSessionLengthInSec((sessionLengthInSec) => {
+            updateTimerDisplay(sessionLengthInSec - 1);
+            return sessionLengthInSec - 1;
+          }),
+        1000
+      );
+      setTimeoutId(timeout);
+    }
+
+    if (start && isBreak && breakLengthInSec === 0) {
+      setIsBreak(false);
+      setBreakLengthInSec(() => breakLength * 60);
+      updateTimerDisplay(sessionLengthInSec);
+      audioRef.current && audioRef.current.play();
+    }
+
+    if (start && isBreak && breakLengthInSec > 0) {
+      const timeout: number = setTimeout(
+        () =>
+          setBreakLengthInSec((breakLengthInSec) => {
+            updateTimerDisplay(breakLengthInSec - 1);
+            return breakLengthInSec - 1;
+          }),
+        1000
+      );
+      setTimeoutId(timeout);
+    }
+  }, [start, isBreak, sessionLengthInSec, breakLengthInSec]);
+
+  function updateTimerDisplay(timeInSec: number): void {
+    const newTime: string = new Date(timeInSec * 1000)
       .toISOString()
       .substring(14, 19);
-    setTimer(newTime);
+    if (timeInSec / 60 === 60) {
+      setTimerDisplay(() => newTime.replace(/^.{2}/g, "60"));
+    } else {
+      setTimerDisplay(() => newTime);
+    }
   }
 
   function handleStartTimerClick() {
-    let seconds: number = sessionLength * 60;
-
-    function handleInterval() {
-      const time: Date = new Date(seconds * 1000);
-      setTimer(time.toISOString().substring(14, 19));
-      time.setSeconds(seconds, 0);
-      seconds -= 1;
+    if (start) {
+      setStart(false);
+      clearTimeout(timeoutId);
+    } else {
+      setStart(true);
     }
-
-    const intervalId: number = setInterval(handleInterval, 1000);
-    setIntervalId(intervalId);
   }
 
   function handleBreakDecrementClick() {
-    if (breakLength > 1 && intervalId === -1) {
-      setBreakLength((breakLength) => breakLength - 1);
+    if (breakLength > 1 && !start) {
+      setBreakLength((breakLength) => {
+        setBreakLengthInSec(() => (breakLength - 1) * 60);
+        return breakLength - 1;
+      });
     }
   }
 
   function handleBreakIncrementClick() {
-    if (breakLength < 60 && intervalId === -1) {
-      setBreakLength((breakLength) => breakLength + 1);
+    if (breakLength < 60 && !start) {
+      setBreakLength((breakLength) => {
+        setBreakLengthInSec(() => (breakLength + 1) * 60);
+        return breakLength + 1;
+      });
     }
   }
 
   function handleSessionDecrementClick() {
-    if (sessionLength > 1 && intervalId === -1) {
-      setSessionLength((sessionLength) => sessionLength - 1);
+    if (sessionLength > 1 && !start) {
+      setSessionLength((sessionLength) => {
+        setSessionLengthInSec(() => (sessionLength - 1) * 60);
+        updateTimerDisplay((sessionLength - 1) * 60);
+        return sessionLength - 1;
+      });
     }
   }
 
   function handleSessionIncrementClick() {
-    if (intervalId === -1) {
-      setSessionLength((sessionLength) => sessionLength + 1);
+    if (sessionLength < 60 && !start) {
+      setSessionLength((sessionLength) => {
+        setSessionLengthInSec(() => (sessionLength + 1) * 60);
+        updateTimerDisplay((sessionLength + 1) * 60);
+        return sessionLength + 1;
+      });
     }
   }
 
   function handleResetClick() {
+    clearTimeout(timeoutId);
+    setStart(false);
+    setIsBreak(false);
     setBreakLength(5);
     setSessionLength(25);
-    setTimer("25:00");
-    clearInterval(intervalId);
-    setIntervalId(-1);
+    setBreakLengthInSec(300), 1000;
+    setSessionLengthInSec(1500), 1000;
+    setTimerDisplay("25:00");
+    audioRef.current && audioRef.current.pause();
+    audioRef.current ? (audioRef.current.currentTime = 0) : null;
   }
 
   return (
@@ -79,7 +135,7 @@ function App() {
         <Button
           id="break-decrement"
           onClick={handleBreakDecrementClick}
-          intervalId={intervalId}
+          isDisabled={start}
         >
           -
         </Button>
@@ -87,7 +143,7 @@ function App() {
         <Button
           id="break-increment"
           onClick={handleBreakIncrementClick}
-          intervalId={intervalId}
+          isDisabled={start}
         >
           +
         </Button>
@@ -97,7 +153,7 @@ function App() {
         <Button
           id="session-decrement"
           onClick={handleSessionDecrementClick}
-          intervalId={intervalId}
+          isDisabled={start}
         >
           -
         </Button>
@@ -105,18 +161,19 @@ function App() {
         <Button
           id="session-increment"
           onClick={handleSessionIncrementClick}
-          intervalId={intervalId}
+          isDisabled={start}
         >
           +
         </Button>
       </section>
       <section className="timer-wrapper">
-        <div id="timer-label">Session</div>
-        <div id="time-left">{timer}</div>
+        <div id="timer-label">{isBreak ? "Break" : "Session"}</div>
+        <div id="time-left">{timerDisplay}</div>
+        <audio id="beep" src="src/assets/beep.mp3" ref={audioRef} />
       </section>
       <section className="timer-controls-wrapper">
         <Button id="start_stop" onClick={handleStartTimerClick}>
-          Start
+          {!start ? "Start" : "Pause"}
         </Button>
         <Button id="reset" onClick={handleResetClick}>
           Reset
@@ -129,17 +186,16 @@ function App() {
 interface Button {
   id: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  intervalId?: number;
+  isDisabled?: boolean;
   children: React.ReactNode;
 }
 
 export function Button({
   id,
   onClick,
-  intervalId = -1,
+  isDisabled = false,
   children,
 }: Button): JSX.Element {
-  const isDisabled = intervalId === -1 ? false : true;
   return (
     <button id={id} onClick={onClick} disabled={isDisabled}>
       {children}
